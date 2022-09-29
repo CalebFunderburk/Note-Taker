934893bae286574e3dd2b9d08c17040f37875388
Caleb <calebfunderburk@icloud.com>
2022-09-29 13:48:13 -0400

    successfully displaying test note

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
+// Database
+const { notes } = require('./db/db.json')
 
 // Format express
 const app = express()
@@ -13,16 +15,23 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
-// Test route
+// Route to display previously saved notes
+app.get('/api/notes', (req, res) => {
+    let savedNotes = fs.readFileSync('./db/db.json', 'utf8')
+    res.json(JSON.parse(savedNotes))
+})
+
+// Route to index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
+// Route to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-// Serve ignition
+// Server ignition
 app.listen(PORT, () => {
     console.log(`You are currently tuned into port ${PORT}`)
 })
